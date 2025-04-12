4021a27695b2bf87f3462a59a130ddcccf095106
sureshb987 <147478858+sureshb987@users.noreply.github.com>
2025-04-12 15:14:13 +0530

    Update Backend.tf

--- Environment/Prod/Backend.tf
+++ Environment/Prod/Backend.tf
@@ -1,1 +1,9 @@
-
+terraform {
+  backend "s3" {
+    bucket         = "your-terraform-state-bucket"
+    key            = "env/${var.environment}/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
