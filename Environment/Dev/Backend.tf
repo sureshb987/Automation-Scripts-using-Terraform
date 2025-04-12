76ec29a9cb226ac0b88cee13742d35497d6ac7d2
sureshb987 <147478858+sureshb987@users.noreply.github.com>
2025-04-12 19:42:28 +0530

    Update Backend.tf

--- Environment/Dev/Backend.tf
+++ Environment/Dev/Backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "your-terraform-state-bucket"
-    key            = "env/${var.environment}/terraform.tfstate"
+    key            = "env/Dev/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
