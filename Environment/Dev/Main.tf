8ac281a91c1eb849d4a8e742bdc3a32f9dbeb101
sureshb987 <147478858+sureshb987@users.noreply.github.com>
2025-04-12 15:11:00 +0530

    Update Main.tf

--- Environment/Dev/Main.tf
+++ Environment/Dev/Main.tf
@@ -1,26 +1,24 @@
-module "vpc" {
-  source        = "../../modules/vpc"
+module "CorporateProject_vpc" {
+  source        = "../../Cluster Server"
   vpc_cidr      = "10.0.0.0/16"
   public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   azs           = ["ap-south-1a", "ap-south-1b"]
-  environment   = "dev"
+  environment   = var.environment
 }
-
-module "eks" {
-  source        = "../../modules/eks"
-  cluster_name  = "dev-eks"
+module "CorporateProject_eks" {
+  source        = "../../Cluster Server"
+  cluster_name  = "${var.environment}-eks"
   k8s_version   = "1.27"
-  subnets       = module.vpc.public_subnets
-  vpc_id        = module.vpc.vpc_id
+  subnets       = module.CorporateProject_vpc.public_subnets
+  vpc_id        = module.CorporateProject_vpc.vpc_id
   key_name      = var.key_name
 }
-
-module "rds" {
-  source        = "../../modules/rds"
-  environment   = "dev"
-  db_name       = "devdb"
-  db_user       = "devuser"
+module "CorporateProject_rds" {
+  source        = "../../Cluster Server"
+  environment   = var.environment
+  db_name       = "${var.environment}db"
+  db_user       = "${var.environment}user"
   db_password   = var.db_password
-  subnet_ids    = module.vpc.public_subnets
-  sg_ids        = [module.eks.cluster_security_group_id]
+  subnet_ids    = module.CorporateProject_vpc.public_subnets
+  sg_ids        = [module.CorporateProject_eks.cluster_security_group_id]
 }
