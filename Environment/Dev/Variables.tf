46b495c32e3a6694d1c59706b7e30cbfeb4a7776
sureshb987 <147478858+sureshb987@users.noreply.github.com>
2025-04-12 15:12:00 +0530

    Update Variables.tf

--- Environment/Dev/Variables.tf
+++ Environment/Dev/Variables.tf
@@ -1,7 +1,15 @@
 variable "key_name" {
   description = "The SSH key name for EC2 instances"
+  type        = string
 }
 
 variable "db_password" {
   description = "The password for the RDS database"
+  type        = string
+  sensitive   = true
+}
+
+variable "environment" {
+  description = "dev environment"
+  type        = string
 }
