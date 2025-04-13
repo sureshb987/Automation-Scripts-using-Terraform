7a712eeef76bec9d333013be0aa00545518356e5
sureshb987 <147478858+sureshb987@users.noreply.github.com>
2025-04-13 12:32:58 +0530

    Update variables.tf

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -3,3 +3,27 @@ variable "ssh_key_name" {
   type        = string
   default     = "Devops project"
 }
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+}
+
+variable "k8s_version" {
+  description = "Kubernetes version"
+  type        = string
+}
+
+variable "subnets" {
+  description = "List of subnet IDs"
+  type        = list(string)
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "key_name" {
+  description = "SSH key name"
+  type        = string
+}
