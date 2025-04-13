cb79a1dec8c93716876a5075ed38f5136d858519
sureshb987 <147478858+sureshb987@users.noreply.github.com>
2025-04-13 12:33:29 +0530

    Update Variable.tf

--- modules/Vpc/Variable.tf
+++ modules/Vpc/Variable.tf
@@ -3,3 +3,22 @@ variable "ssh_key_name" {
   type        = string
   default     = "Devops project"
 }
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "azs" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+}
