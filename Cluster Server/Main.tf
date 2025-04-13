06894a0dde4c4a20e51f9bdbb532c388af414269
sureshb987 <147478858+sureshb987@users.noreply.github.com>
2025-04-13 13:12:47 +0530

    Update Main.tf

--- Cluster Server/Main.tf
+++ Cluster Server/Main.tf
@@ -270,7 +270,46 @@ resource "aws_db_instance" "corporateproject_mysql" {
     Name = "CorporateProject MySQL DB"
   }
 }
+resource "aws_s3_bucket" "SB3" {
+  bucket = "sb3bucket33"  # Bucket names must be globally unique and lowercase
+}
 
+resource "aws_s3_bucket_ownership_controls" "SB4" {
+  bucket = aws_s3_bucket.SB3.id
+
+  rule {
+    object_ownership ="BucketOwnerPreferred"  # Valid values: BucketOwnerEnforced, BucketOwnerPreferred, ObjectWriter
+  }
+}
+
+resource "aws_s3_bucket_acl" "SB5" {
+  depends_on = [aws_s3_bucket_ownership_controls.SB4]
+  bucket     = aws_s3_bucket.SB3.id
+  acl        = "private"  # Valid ACL values: private, public-read, public-read-write, authenticated-read, etc.
+}
+
+resource "aws_s3_bucket_versioning" "SB6" {
+  bucket = aws_s3_bucket.SB3.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-locks"
+  hash_key     = "LockID"
+  billing_mode = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Environment = "dev"
+    Name        = "Terraform State Lock Table"
+  }
+}
 resource "aws_iam_role_policy_attachment" "CorporateProject_node_group_role_policy" {
   role       = aws_iam_role.CorporateProject_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
