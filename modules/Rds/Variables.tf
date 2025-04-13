aaf924e5c845ecb58039fd09c6f27a3691c201b5
sureshb987 <147478858+sureshb987@users.noreply.github.com>
2025-04-13 12:31:31 +0530

    Update Variables.tf

--- modules/Rds/Variables.tf
+++ modules/Rds/Variables.tf
@@ -3,3 +3,33 @@ variable "ssh_key_name" {
   type        = string
   default     = "Devops project"
 }
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+}
+
+variable "db_name" {
+  description = "Database name"
+  type        = string
+}
+
+variable "db_user" {
+  description = "Database username"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Database password"
+  type        = string
+  sensitive   = true
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs"
+  type        = list(string)
+}
+
+variable "sg_ids" {
+  description = "List of security group IDs"
+  type        = list(string)
+}
