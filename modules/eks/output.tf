7dcdbedec5a28e565c2d54c55c1d24afa7e41451
sureshb987 <147478858+sureshb987@users.noreply.github.com>
2025-04-14 10:44:36 +0530

    Update output.tf

--- modules/eks/output.tf
+++ modules/eks/output.tf
@@ -1,7 +1,8 @@
-output "cluster_id" {
-  value = aws_eks_cluster.CorporateProject.id
+output "eks_cluster_name" {
+  value = aws_eks_cluster.eks_cluster.name
 }
 
-output "node_group_id" {
-  value = aws_eks_node_group.CorporateProject.id
+output "eks_node_group_name" {
+  value = aws_eks_node_group.eks_nodes.node_group_name
 }
+
